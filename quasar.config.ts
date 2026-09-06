33d6ace733bd36423d11d24c4879124b8d4214c5
Hans <hans@email.com>
2026-09-06 16:52:21 +0200

    Use stable IPv4 API proxy

--- quasar.config.ts
+++ quasar.config.ts
@@ -25,7 +25,7 @@ export default function () {
       port: 9000,
       open: false,
       proxy: {
-        '/api': 'http://localhost:3000'
+        '/api': 'http://127.0.0.1:3000'
       }
     },
 
